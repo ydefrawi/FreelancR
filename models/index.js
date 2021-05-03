34274d30b897bdddf478401c219016f2b90f282d
Brian Bellanger <brianjbellanger@gmail.com>
2021-05-03 13:14:42 -0500

    B-AddedBidTable

--- models/index.js
+++ models/index.js
@@ -1,13 +1,33 @@
 const User = require('./User');
 const Project = require('./Project');
+const Bid = require('./Bid');
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
+User.hasMany(Bid, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.hasMany(Bid, {
+  foreignKey: 'project_id',
+  onDelete: 'CASCADE'
+});
+
+
 Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Bid.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Bid.belongsTo(Project, {
+  foreignKey: 'project_id'
+});
+
+module.exports = { User, Project, Bid };
