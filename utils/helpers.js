264ed29243a1a24667a1630ade793c75d34754eb
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-04 20:00:54 -0500

    fixed merge conflict, re-added helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,5 +3,8 @@ module.exports = {
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
       new Date(date).getFullYear()
     }`;
+  }, format_amount: (amount) => {
+    // format large numbers with commas
+    return parseInt(amount).toLocaleString();
   }
 };
