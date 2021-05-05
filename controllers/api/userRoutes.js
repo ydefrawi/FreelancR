49bd599e4d59fc8421ac4a68b683bd770452d8dc
Brian Bellanger <brianjbellanger@gmail.com>
2021-05-05 08:59:04 -0500

    Added tier to the session variable for login and signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -7,6 +7,7 @@ router.post('/', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.tier = userData.tier;
       req.session.logged_in = true;
 
       res.status(200).json(userData);
@@ -38,6 +39,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.tier = userData.tier;
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
